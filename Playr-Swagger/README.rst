6f58dc9767d460601ffcb7699de4695c97f57b1f
Olivier Samyn <olivier@26lights.com>
2014-05-07 17:44:48 +0200

    Import playr-swagger

---
+++ Playr-Swagger/README.rst
@@ -0,0 +1,84 @@
+================
+Play'R - Swagger
+================
+
+Generate Swagger documentation for your Play'R defined ReST API.
+
+
+This is an extension to the `Play'R project <https://github.com/26lights/PlayR>`_ 
+
+.. warning::
+
+  This project depends on a checkout of the swagger-ui project.
+
+  To get this dependency, you have to run the following commands ::
+  
+    $ git clone https://github.com/26lights/PlayR-swagger.git
+    $ cd PlayR-swagger
+    $ git submodule update
+
+How to use it
+=============
+
+
+First, you have to add ``playr-swagger`` to your build dependencies ( ``build.sbt`` ):
+
+.. code-block:: scala
+
+  resolvers += "26Lights snapshots" at "http://build.26source.org/nexus/content/repositories/public-snapshots"
+
+  libraryDependencies += "26lights"  %% "playr-swagger"  % "0.2.0-SNAPSHOT"
+
+
+Next, create an instance of ``SwaggerRestDocumentation`` for an existing Play'R router:
+
+.. code-block:: scala
+
+  ...
+  import twentysix.playr.swagger.SwaggerRestDocumentation 
+  ...
+    val apidocs = new SwaggerRestDocumentation(router) // Generate swagger documentation
+  ...
+
+Example using ``playr-tutorial`` project's ``Application`` file:
+
+.. code-block:: scala
+
+  package controllers
+  
+  import play.api._
+  import play.api.mvc._
+  import twentysix.playr._
+  import twentysix.playr.swagger.SwaggerRestDocumentation // Import 
+  
+  object Application extends Controller {
+  
+    val crmApi = RestApiRouter()
+      .add(PersonController)
+      .add(new RestResourceRouter(CompanyController)
+        .add("employee", company => EmployeeController(company))
+        .add("functions", "GET", CompanyController.functions)
+      )
+  
+    val api = RestApiRouter()
+      .add("crm" -> crmApi)
+      .add(new RestResourceRouter(ColorController))
+  
+    val apidocs = new SwaggerRestDocumentation(api)
+  }
+
+
+And finally, just add a reference to the ``apidocs`` in your routes files.
+
+Using the ``playr-tutorial`` project again: 
+
+.. code-block:: nginx
+
+  # Routes
+  # This file defines all application routes (Higher priority routes first)
+  # ~~~~
+
+  ->      /api                          controllers.Application.api
+  ->      /api-docs                     controllers.Application.apidocs
+
+While running your application, if you point your browser to ``/api-docs``, you will get the swagger-ui interface with the generated documentation of your Play'R defined ReST api.
---
+++ Playr-Swagger/app/twentysix/playr/swagger/SwaggerRestDocumentation.scala
@@ -0,0 +1,121 @@
+package twentysix.playr.swagger
+
+import play.core.Router
+import scala.runtime.AbstractPartialFunction
+import scala.reflect.runtime.universe._
+import play.api.mvc._
+import play.api.libs.json._
+import play.api.Logger
+import twentysix.playr._
+
+case class SwaggerResource(path: String, description: String)
+object SwaggerResource {
+  implicit val jsonWrites = Json.writes[SwaggerResource]
+}
+
+case class SwaggerParameter(name: String, description: String, paramType: String="path", `type`: String="string", required: Boolean=true)
+object SwaggerParameter {
+  implicit val jsonWrites = Json.writes[SwaggerParameter]
+}
+
+case class SwaggerOperation(method: HttpMethod,
+                            nickname: String,
+                            summary: String,
+                            parameters: Seq[SwaggerParameter],
+                            `type`: String="string",
+                            consumes: Seq[String] = Seq("application/json", "application/xml", "text/plain"))
+object SwaggerOperation {
+  implicit val httpMethodJsonWrites = new Writes[HttpMethod] {
+    def writes(method: HttpMethod) = JsString(method.name)
+  }
+  implicit val jsonWrites = Json.writes[SwaggerOperation]
+  def simple(method: HttpMethod, nickname: String, parameters: Seq[SwaggerParameter]) = new SwaggerOperation(method, nickname, nickname, parameters)
+}
+
+case class SwaggerApi(path: String, description: String, operations: Traversable[SwaggerOperation])
+object SwaggerApi {
+  implicit val jsonWrites = Json.writes[SwaggerApi]
+}
+
+class SwaggerRestDocumentation(val restApi: RestRouter, val apiVersion: String="1.2") extends SimpleRouter {
+  private val SubPathExpression = "^(/([^/]+)).*$".r
+
+  val apiMap = restApi.routeResources("").map{ info =>
+    info.path -> info
+  }.toMap
+
+  def apiList = apiMap.map { case(path, info) =>
+    SwaggerResource(path, info.resourceType.toString())
+  }
+
+  def operationList(path: String, routeInfo: RestRouteInfo, parameters: Seq[SwaggerParameter] = Seq()): List[SwaggerApi] = {
+
+    val bodyParam = SwaggerParameter("body", "body", "body")
+
+    var res = List[SwaggerApi]()
+    var ops = routeInfo.caps.flatMap{ caps =>
+      caps match {
+        case ResourceCaps.Read   => Some(SwaggerOperation.simple(GET, s"List ${routeInfo.name}", parameters))
+        case ResourceCaps.Create => Some(SwaggerOperation.simple(POST, s"Create ${routeInfo.name}", parameters :+ bodyParam))
+        case ResourceCaps.Action =>
+          val method = routeInfo.asInstanceOf[ActionRestRouteInfo].method
+          Some(SwaggerOperation.simple(method, routeInfo.name, if (Seq(POST, PUT, PATCH).contains(method)) parameters :+ bodyParam else parameters))
+        case _ => None
+      }
+    }
+    if(!ops.isEmpty)
+      res = res :+ new SwaggerApi(path, "Generic operations", ops)
+
+    val subParams = parameters :+ SwaggerParameter(s"${routeInfo.name}_id", s"identified ${routeInfo.name}")
+    ops = routeInfo.caps.flatMap{ caps =>
+      caps match {
+        case ResourceCaps.Read   => Some(SwaggerOperation.simple(GET, s"Read ${routeInfo.name}", subParams))
+        case ResourceCaps.Write  => Some(SwaggerOperation.simple(PUT, s"Write ${routeInfo.name}", subParams :+ bodyParam))
+        case ResourceCaps.Update => Some(SwaggerOperation.simple(PATCH, s"Update ${routeInfo.name}", subParams :+ bodyParam))
+        case ResourceCaps.Delete => Some(SwaggerOperation.simple(DELETE, s"Delete ${routeInfo.name}", subParams))
+        case _ => None
+      }
+    }
+    if(!ops.isEmpty)
+      res = res :+ new SwaggerApi(s"$path/{${routeInfo.name}_id}", "Operations on identified resource", ops)
+
+    res ++ routeInfo.subResources.flatMap(info => operationList(s"$path/{${routeInfo.name}_id}/${info.name}", info, subParams))
+  }
+
+  def resourceListing = Action {
+    val res = Json.obj(
+        "apiVersion" -> apiVersion,
+        "swaggerVersion" -> "1.2",
+        "apis" -> apiList
+    )
+    Results.Ok(Json.toJson(res))
+  }
+
+  def renderSwaggerUi = Action {
+    Results.Ok(views.html.swagger(this.prefix+".json", this.prefix+"/ui"))
+  }
+
+  def resourceDesc(path: String, routeInfo: RestRouteInfo) = Action {
+    val res = Json.obj(
+        "apiVersion" -> apiVersion,
+        "swaggerVersion" -> "1.2",
+        "basePath" -> restApi.prefix,
+        "resourcePath" -> path,
+        "apis" -> operationList(path, routeInfo)
+    )
+    Results.Ok(Json.toJson(res))
+  }
+
+  private val ApiListing = "^\\.json(/.*)$".r
+  private val UiAsset = "^/ui/(.*)$".r
+
+  def routeRequest(header: RequestHeader, path: String, method: String): Option[Handler] = {
+      path match {
+        case ".json"         => Some(resourceListing)
+        case ""|"/"          => Some(renderSwaggerUi)
+        case ApiListing(api) => apiMap.get(api).map(resourceDesc(api, _))
+        case UiAsset(asset)  => Some(controllers.Assets.at("/public/swagger-ui/dist", asset))
+        case _               => None
+      }
+  }
+}
